f263eb2bc0b48b59500d2cd600a5b51b9e4c9cbc
Farhan Irani <farhanirani007@gmail.com>
2020-04-03 09:31:31 +0530

    .

--- smallgame.py
+++ smallgame.py
@@ -1,4 +1,5 @@
-import pygame
+import pygame,os
+os.chdir('first')
 pygame.init()  
 
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
