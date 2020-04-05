a76b72ea3010eb95059a85dc4f3a7229d4278ce8
Farhan Irani <farhanirani007@gmail.com>
2020-04-05 21:52:35 +0530

    20%

--- snakegame.py
+++ snakegame.py
@@ -8,7 +8,7 @@ clock = pygame.time.Clock()
 snakeimg = pygame.image.load("snake.png")
 pygame.display.set_icon(snakeimg)
 
-
+13, 120, 13
 class snakehead:
     def __init__(self,x,y):
         self.x = x
@@ -28,14 +28,31 @@ class snakehead:
         pygame.draw.rect(win, (0,255,0), ( (self.x)*(500/rows), (self.y)*(500/columns), (500/columns), (500/rows) ))
 
 
+class snakebody:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def draw(self):
+        if self.x < columns-1 and direction == 1:
+            self.x += 1
+        elif self.y < rows-1 and direction == 2:
+            self.y += 1
+        elif self.x > 0 and direction == 3:
+            self.x -= 1
+        elif self.y > 0 and direction == 4:
+            self.y -= 1
+
+
+
 
 def drawBoard():
     global rows
     global columns
     for i in range(rows):
-        pygame.draw.line(win, (255,0,0), ((i+1)*(500/rows), 0), ((i+1)*(500/rows), 500), 1)
+        pygame.draw.line(win, (110, 73, 13), ((i+1)*(500/rows), 0), ((i+1)*(500/rows), 500), 1)
     for i in range(columns):
-        pygame.draw.line(win, (255,0,0), (0, (i+1)*(500/rows)), (500, (i+1)*(500/rows)), 1)
+        pygame.draw.line(win, (110, 73, 13), (0, (i+1)*(500/rows)), (500, (i+1)*(500/rows)), 1)
 
 def redraw():
     win.fill((100,10,10))
@@ -58,21 +75,14 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-
-    # if delaycounter == 0:
     if keys[pygame.K_LEFT] and direction != 3 and direction != 1:
         direction = 3
-        # delaycounter = 1
     if keys[pygame.K_RIGHT] and direction != 1 and direction != 3:
         direction = 1
-        # delaycounter = 1
     if keys[pygame.K_UP] and direction != 4 and direction != 2:
         direction = 4
-        # delaycounter = 1
     if keys[pygame.K_DOWN] and direction != 2 and direction != 4:
         direction = 2
-        # delaycounter = 1
-
 
     redraw()
 
