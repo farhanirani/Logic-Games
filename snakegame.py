f31ca6b1ed934ff2c43ad08d4967b3f0413e54bd
Farhan Irani <farhanirani007@gmail.com>
2020-04-06 21:45:02 +0530

    94%?

--- snakegame.py
+++ snakegame.py
@@ -15,13 +15,13 @@ class snakehead:
         self.y = y
 
     def draw(self):
-        if self.x < columns-1 and direction == 1:
+        if self.x < columns-1 and gdirection == 1:
             self.x += 1
-        elif self.y < rows-1 and direction == 2:
+        elif self.y < rows-1 and gdirection == 2:
             self.y += 1
-        elif self.x > 0 and direction == 3:
+        elif self.x > 0 and gdirection == 3:
             self.x -= 1
-        elif self.y > 0 and direction == 4:
+        elif self.y > 0 and gdirection == 4:
             self.y -= 1
         else:
             pass
@@ -29,10 +29,10 @@ class snakehead:
 
 
 class snakebody:
-    def __init__(self,x,y,direction):
+    def __init__(self,x,y,directiond):
         self.x = x
         self.y = y
-        self.direction = direction
+        self.direction = directiond
     
     def draw(self):
         if self.x < columns-1 and self.direction == 1:
@@ -43,6 +43,8 @@ class snakebody:
             self.x -= 1
         elif self.y > 0 and self.direction == 4:
             self.y -= 1
+        else:
+            pass
         pygame.draw.rect(win, (13, 120, 13), ( (self.x)*(500/rows), (self.y)*(500/columns), (500/columns), (500/rows) ))
 
 
@@ -55,16 +57,18 @@ def drawBoard():
     for i in range(columns):
         pygame.draw.line(win, (110, 73, 13), (0, (i+1)*(500/rows)), (500, (i+1)*(500/rows)), 1)
 
+
 def redraw():
     win.fill((100,10,10))
     drawBoard()
     snake.draw()
-    previousdir = direction
+
+    nextdirection = int(previousgdirection)
     for b in snakearray:
-        temp = int(previousdir)
-        b.direction = previousdir
+        temp = int(b.direction)
+        b.direction = int(nextdirection)
         b.draw()
-        previousdir = temp
+        nextdirection = int(temp)
     pygame.display.update()
 
 
@@ -72,29 +76,33 @@ def redraw():
 snakearray = []
 snakearray.append(snakebody(9,10,1)) 
 snakearray.append(snakebody(8,10,1))
-previousdir = 1
-direction = 1
+snakearray.append(snakebody(7,10,1))
+previousgdirection = 1
+gdirection = 1
+
 snake = snakehead(10,10)
 rows = 20
 columns = 20
 run = True
 while run:
-    clock.tick(40)
+    clock.tick(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and direction != 3 and direction != 1:
-        direction = 3
-    if keys[pygame.K_RIGHT] and direction != 1 and direction != 3:
-        direction = 1
-    if keys[pygame.K_UP] and direction != 4 and direction != 2:
-        direction = 4
-    if keys[pygame.K_DOWN] and direction != 2 and direction != 4:
-        direction = 2
+    previousgdirection = int(gdirection)
+    if keys[pygame.K_LEFT] and gdirection != 3 and gdirection != 1:
+        gdirection = 3
+    if keys[pygame.K_RIGHT] and gdirection != 1 and gdirection != 3:
+        gdirection = 1
+    if keys[pygame.K_UP] and gdirection != 4 and gdirection != 2:
+        gdirection = 4
+    if keys[pygame.K_DOWN] and gdirection != 2 and gdirection != 4:
+        gdirection = 2
 
+    
     redraw()
 
 
