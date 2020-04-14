b91c398204d2a0855d2b6c338caf3873776f8b75
Farhan Irani <farhanirani007@gmail.com>
2020-04-14 19:40:48 +0530

    .

--- particles-explosion.py
+++ particles-explosion.py
@@ -16,7 +16,7 @@ while True:
     mx, my = pygame.mouse.get_pos()
     #                  spawn x,y                   speed x, speed y               life timer and radius                        
     
-    for _ in range(5):
+    for _ in range(8):
         # particles.append([ [mx, my], [random.randint(0, 20) / 10 - 1, -2], random.randint(4,8)])
         particles.append([ [mx, my], [random.randint(0, 20) / 10 - 1, random.randint(0, 20) / 10 - 1], random.randint(4,8)])
 
@@ -28,7 +28,7 @@ while True:
         # gravity
         particle[1][1] += 0.1
         
-        pygame.draw.circle(screen, (255,255,255), [ int(particle[0][0]), int(particle[0][1]) ], int(particle[2]))
+        pygame.draw.circle(screen, (80,random.randint(1,80),random.randint(1,255)), [ int(particle[0][0]), int(particle[0][1]) ], int(particle[2]))
 
     for particle in particles:
         if particle[2] <= 0:
