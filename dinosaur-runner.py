2cb48bfd2aec4031b699079af36ff4735b7489cf
Farhan Irani <farhanirani007@gmail.com>
2020-04-20 20:40:25 +0530

    done

--- dinosaur-runner.py
+++ dinosaur-runner.py
@@ -11,6 +11,28 @@ pygame.display.set_icon(logoIMG)
 screen = pygame.display.set_mode((1000, 600),0,32)
 bottomimg = pygame.image.load("bottom.png")
 
+cactusIMG = pygame.image.load("cactus.jpg").convert()
+cactusIMG.set_colorkey((255,255,255))
+
+class cactus:
+    def __init__(self):
+        self.scalelen = random.randint(30, 90)
+        self.IMG = pygame.transform.scale(cactusIMG,( int(self.scalelen / 2), self.scalelen))
+        if random.randint(1,2) == 1:
+            pass
+        else:
+            self.IMG = pygame.transform.flip(self.IMG, True, False)
+        self.num = random.randint(1,3)
+        self.x = 970
+    
+    def draw(self):
+        global movementSpeed
+        for i in range(self.num):
+            screen.blit(self.IMG, ( self.x + (i* self.IMG.get_size()[0] ) , 310 - self.IMG.get_size()[1] ) )
+        # pygame.draw.rect(screen, (255,0,0), (self.x, 310-self.IMG.get_size()[1], self.num*self.IMG.get_size()[0], self.IMG.get_size()[1] ), 1)
+        self.x -= movementSpeed
+        
+
 
 def drawTimer():
     global ShootTimer
@@ -22,9 +44,27 @@ def drawTimer():
     pygame.draw.rect(screen, (97, 169, 244), (15,15, int(tempLength),10))
 
 
+def gameover():
+    gameoverIMG = pygame.image.load("gameover.jpg").convert()
+    gameoverIMG.set_colorkey((255,255,255))
+    gameoverIMG = pygame.transform.scale(gameoverIMG, (200,12))
+    screen.blit(gameoverIMG, (400, 50))
+    
+    screen.blit(images[4], (int(xdino), int(ydino)))
+    pygame.display.update()
+    i = 0
+    while i < 400:
+        pygame.time.delay(10)
+        i += 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+    pygame.quit()
+    sys.exit()
+
 
 def redraw():
-    global movementCount
     drawTimer()
     screen.blit(bottomimg,(0,350))
     pygame.display.update()
@@ -60,18 +100,27 @@ for i in range(1,6):
 
 particles = []
 groundparticles = []
+blastparticles = []
+cactusobjects = []
+
+cactustimer = 0
 groundparticlestimer = 0
 ShootTimer = 0
-ShootTime = 100
+ShootTime = 200
 isJump = False
 isDuck = False
 JumpTimer = 25
+
+groundx = 500
+groundx2 = 500
 
 xdino = 80
 ydino = 250
 ydinotemp = 250
 
 movementCount = 0
+movementSpeed = 6
+neg = 0
 
 startGame()
 while True:
@@ -88,18 +137,19 @@ while True:
                 pygame.quit()
                 sys.exit()
 
-        #jump
-        if event.type == pygame.KEYDOWN and not isJump:
-            if event.key == K_SPACE:
-                isJump = True
-                movementCount = 0
-
         #duck
         keys = pygame.key.get_pressed()
         if keys[pygame.K_DOWN]:     
             isDuck = True
         else:
             isDuck = False
+
+        #jump
+        if event.type == pygame.KEYDOWN and not isJump:
+            if event.key == K_SPACE or event.key == K_UP:
+                isJump = True
+                isDuck = False
+                movementCount = 0
 
         # blast
         if event.type == pygame.KEYDOWN and ShootTimer == 0:
@@ -113,17 +163,21 @@ while True:
 
     #-------------------end of key pressed
     # draw ground
-    pygame.draw.line(screen, (83, 83, 83), (xdino-50, 250+60), (970,250+60), 1)
+    if groundx > xdino - 50:
+        groundx -= 10
+    if groundx2 < 970:
+        groundx2 += 10
+    pygame.draw.line(screen, (83, 83, 83), (groundx, 250+60), (groundx2,250+60), 1)
 
     if groundparticlestimer == 0:
         groundparticles.append( [ 967 , random.randint(315,330) , random.randint(1,8) , random.randint(1,2) ])
-        groundparticlestimer = random.randint(0,15)
+        groundparticlestimer = random.randint(0,6)
     else:
         groundparticlestimer -= 1
 
     for gp in groundparticles:
         pygame.draw.rect(screen, (83, 83, 83), (gp[0], gp[1], gp[2], gp[3]) )
-        gp[0] -= 4
+        gp[0] -= movementSpeed
     
     for gp in groundparticles:
         if gp[0] < 40:
@@ -133,33 +187,41 @@ while True:
     #jump
     if isJump:
         if JumpTimer >= -25:
-            neg = 1
-            if JumpTimer < 0:
-                neg = -1
-                if isDuck:
-                    neg = -6
+            if neg != -6:
+                neg = 1
+                if JumpTimer < 0:
+                    neg = -1
+                    if isDuck:
+                        neg = -6
             if ydino - (JumpTimer ** 2) * 0.035 * neg < ydinotemp:
                 ydino -= (JumpTimer ** 2) * 0.035 * neg
             else:
                 ydino = ydinotemp * 1
+                isJump = False
+                JumpTimer = 25
+                neg = 1
+            
             JumpTimer -= 1
         else:
             isJump = False
             JumpTimer = 25
+            neg = 1
 
         screen.blit(images[0], (int(xdino), int(ydino)))
-        pygame.draw.rect(screen, (255,0,0), (xdino+4,ydino+4,50,50), 1)
-        
+        # pygame.draw.rect(screen, (255,0,0), (xdino+4,ydino+4,50,50), 1)
+
+    #duck    
     elif isDuck:
         movementCount += 1
         if movementCount % 20 < 10:
-            screen.blit(duckimages[0], (int(xdino), int(ydino)))
-        else:
             screen.blit(duckimages[1], (int(xdino), int(ydino)))
+        else:
+            screen.blit(duckimages[0], (int(xdino), int(ydino)))
 
-        pygame.draw.rect(screen, (255,0,0), (xdino+4,ydino+30,80,40), 1)
+        # pygame.draw.rect(screen, (255,0,0), (xdino+4,ydino+30,80,20), 1)
 
-    else: # else move right obviously
+    # else move right obviously
+    else: 
         movementCount += 1
         # animation
         if movementCount % 20 < 10:
@@ -167,7 +229,7 @@ while True:
         else:
             screen.blit(images[3], (int(xdino), int(ydino)))
 
-        pygame.draw.rect(screen, (255,0,0), (xdino+4,ydino+4,50,50), 1)
+        # pygame.draw.rect(screen, (255,255,0), (xdino+4,ydino+4,50,50), 1)
 
 
     # blast
@@ -187,6 +249,64 @@ while True:
 
 
 
+    #cactus
+    if cactustimer == 0:
+        cactusobjects.append( cactus() )
+        cactustimer = random.randint(60,140)
+    else:
+        cactustimer -= 1
+
+    for cac in cactusobjects:
+        cac.draw()
+    for cac in cactusobjects:
+        if cac.x < -100:
+            cactusobjects.remove(cac)
+
+
+    # check for collision
+    # if isDuck:
+    #     for cac in cactusobjects:
+    #         if xdino+4+80 < cac.x  or xdino+4 > cac.x + cac.IMG.get_size()[0] or ydino+4+50 < 310-cac.IMG.get_size()[1] :
+    #             pass
+    #         else:
+    #             gameover()
+    # else:
+    for cac in cactusobjects:
+        if xdino+4+50 + movementSpeed < cac.x  or xdino+4 > cac.x + cac.num * cac.IMG.get_size()[0] or ydino+4+50 < 310-cac.IMG.get_size()[1] :
+            pass
+        else:
+            gameover()
+
+
+    # check for blast hit
+    run = 1
+    for particle in particles:
+        if run == 1:
+            if particle[3] == 2:
+                for cac in cactusobjects:
+                    if particle[0][0] + 40 > cac.x and particle[0][0] + 40 < cac.x+cac.IMG.get_size()[0]  and particle[0][1] > 310-cac.IMG.get_size()[1] and run == 1:
+                        for i in range(30):
+                            blastparticles.append([ [cac.x +( cac.IMG.get_size()[0] / 2), 310-cac.IMG.get_size()[1] +(cac.IMG.get_size()[1] / 2)], [random.randint(0, 20) / 10 - 1, random.randint(0, 20) / 10 - 1], random.randint(4,8)])
+                        cactusobjects.remove(cac)
+                        particles.remove(particle)
+                        run = 0
+                    else:
+                        break
+        else:
+            break
     
+
+    # draw cactus explosion
+    for blastparticle in blastparticles:
+        blastparticle[0][0] += blastparticle[1][0]
+        blastparticle[0][1] += blastparticle[1][1]
+        blastparticle[2] -= 0.1
+        pygame.draw.circle(screen, (83, 83, 83), [ int(blastparticle[0][0]), int(blastparticle[0][1]) ], int(blastparticle[2]))
+
+    for blastparticle in blastparticles:
+        if blastparticle[2] <= 0:
+            blastparticles.remove(blastparticle)
+
+
     redraw()
     mainClock.tick(60)
