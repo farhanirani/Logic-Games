43e5c44bebd4670ddd7fcdc6071d471eded14486
Farhan Irani <farhanirani007@gmail.com>
2020-04-24 01:45:38 +0530

    .

--- particles-explosion.py
+++ particles-explosion.py
@@ -4,7 +4,7 @@ pygame.init()
 
 mainClock = pygame.time.Clock()
 pygame.display.set_caption("particle-explosion")
-screen = pygame.display.set_mode((600, 600),0,32)
+screen = pygame.display.set_mode((1000, 800),0,32)
 
 particles = []
 
@@ -16,9 +16,9 @@ while True:
     mx, my = pygame.mouse.get_pos()
     #                  spawn x,y                   speed x, speed y               life timer and radius                        
     
-    for _ in range(8):
+    for _ in range(30):
         # particles.append([ [mx, my], [random.randint(0, 20) / 10 - 1, -2], random.randint(4,8)])
-        particles.append([ [mx, my], [random.randint(0, 20) / 10 - 1, random.randint(0, 20) / 10 - 1], random.randint(4,8)])
+        particles.append([ [mx, my], [random.randint(-10, 10) , random.randint(-10, 10)], random.randint(4,8), ((random.randint(1,255),0,0))])
 
     for particle in particles:
         particle[0][0] += particle[1][0]
@@ -26,9 +26,9 @@ while True:
         particle[2] -= 0.1
         
         # gravity
-        particle[1][1] += 0.1
+        # particle[1][1] += 0.1
         
-        pygame.draw.circle(screen, (random.randint(1,255),0,0), [ int(particle[0][0]), int(particle[0][1]) ], int(particle[2]))
+        pygame.draw.circle(screen, particle[3], [ int(particle[0][0]), int(particle[0][1]) ], int(particle[2]))
 
     for particle in particles:
         if particle[2] <= 0:
